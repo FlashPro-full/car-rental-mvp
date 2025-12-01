0db9efecb11c22b3277fc045f57021dd3d89bd2c
FlashPro-full <flashing.pro@outlook.com>
2025-12-01 14:09:12 -0800

    fix: Updated the date

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -71,7 +71,7 @@ export default function Footer() {
           </div>
         </div>
         <div className="border-t border-gray-700 mt-8 pt-8 text-center text-sm text-gray-400">
-          <p>&copy; 2024 Car Rental Management System. All rights reserved.</p>
+          <p>&copy; 2025 Car Rental Management System. All rights reserved.</p>
         </div>
       </div>
     </footer>
